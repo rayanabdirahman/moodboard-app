1037b9c921f6033b45a93e4a79decf701acfa94b
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-03-12 17:23:17 +0000

    Add custom button styles

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,14 +1,22 @@
-import { IButtonProps as NBButtonProps } from 'native-base';
+import { TouchableOpacityProps as RNTouchableOpacityProps } from 'react-native';
 import * as styled from './styled';
 
-export type Props = NBButtonProps & {
+export type Props = RNTouchableOpacityProps & {
   primary?: boolean;
   secondary?: boolean;
-  // outlined?: boolean;
+  outlined?: boolean;
 };
 
 const Button: React.FC<Props> = ({ children, ...props }) => (
-  <styled.button {...props}>{children}</styled.button>
+  <styled.button {...props}>
+    <styled.text
+      primary={props.primary}
+      secondary={props.secondary}
+      outlined={props.outlined}
+    >
+      {children}
+    </styled.text>
+  </styled.button>
 );
 
 export default Button;
--- src/components/atoms/Button/styled.ts
+++ src/components/atoms/Button/styled.ts
@@ -1,18 +1,34 @@
 import styled, { css } from 'styled-components/native';
-import { Button as NBButton } from 'native-base';
+import { TouchableOpacity, Text } from 'react-native';
 import { Props } from '.';
 
-export const button = styled(NBButton)<Props>`
-  font-family: ${({ theme }) => theme.font.regular};
+export const button = styled(TouchableOpacity)<Props>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: ${({ theme, secondary }) =>
     secondary ? theme.color.black : theme.color.brand};
+  border-radius: 4px;
+  border-width: 2px;
+  border-color: ${({ theme, secondary }) =>
+    secondary ? theme.color.black : theme.color.brand};
+  padding: 8px 0;
 
-  ${({ theme, variant, secondary }) =>
-    variant === 'outline' &&
+  ${({ theme, outlined, secondary }) =>
+    outlined &&
     css`
       background-color: ${theme.color.white};
-      border-width: 2px;
       border-color: ${secondary ? theme.color.black : theme.color.brand};
+    `}
+`;
+
+export const text = styled(Text)<Props>`
+  font-family: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.color.white};
+
+  ${({ theme, outlined, secondary }) =>
+    outlined &&
+    css`
       color: ${secondary ? theme.color.black : theme.color.brand};
     `}
 `;
