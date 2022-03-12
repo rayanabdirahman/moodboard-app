e10966a48a594da1bcce6b2743277ac1250dfa3c
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-03-12 22:13:21 +0000

    add strong typing for nested signup navigator

--- src/navigation/AuthNavigation/index.tsx
+++ src/navigation/AuthNavigation/index.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-
-import { AuthStack, AuthRoutes } from './routes';
-
+import { AuthStack, AuthRoutes, SignUpStack, SignUpRoutes } from './routes';
 import SignUpScreen from '../../screens/Auth/SignUp';
 import SignInScreen from '../../screens/Auth/SignIn';
+
+const screenOptions = {
+  headerTitle: '',
+  headerShadowVisible: false,
+  headerLeft: () => null
+};
+
+const SignUpNavigation = (): React.ReactElement => {
+  return (
+    <SignUpStack.Navigator
+      initialRouteName={SignUpRoutes.SPLASH}
+      screenOptions={screenOptions}
+    >
+      <SignUpStack.Screen name={SignUpRoutes.SPLASH} component={SignUpScreen} />
+    </SignUpStack.Navigator>
+  );
+};
 
 const AuthNavigation = (): React.ReactElement => {
   return (
     <AuthStack.Navigator
       initialRouteName={AuthRoutes.SIGN_UP}
-      screenOptions={{
-        headerTitle: '',
-        headerShadowVisible: false,
-        headerLeft: () => null
-      }}
+      screenOptions={screenOptions}
     >
-      <AuthStack.Screen name={AuthRoutes.SIGN_UP} component={SignUpScreen} />
+      <AuthStack.Screen
+        name={AuthRoutes.SIGN_UP}
+        component={SignUpNavigation}
+      />
       <AuthStack.Screen name={AuthRoutes.SIGN_IN} component={SignInScreen} />
     </AuthStack.Navigator>
   );
--- src/navigation/AuthNavigation/routes.ts
+++ src/navigation/AuthNavigation/routes.ts
@@ -1,3 +1,4 @@
+import { NavigatorScreenParams } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 export enum AuthRoutes {
@@ -5,9 +6,27 @@ export enum AuthRoutes {
   SIGN_IN = 'SignIn'
 }
 
+export enum SignUpRoutes {
+  SPLASH = 'SignUpSplash',
+  BIRTHDAY = 'SignUpBirthday',
+  EMAIL = 'SignUpEmail',
+  PASSWORD = 'SignUpPassword',
+  USERNAME = 'SignUpUsername'
+}
+
+export type SignUpStackParamList = {
+  [SignUpRoutes.SPLASH]: undefined;
+  [SignUpRoutes.BIRTHDAY]: undefined;
+  [SignUpRoutes.EMAIL]: undefined;
+  [SignUpRoutes.PASSWORD]: undefined;
+  [SignUpRoutes.USERNAME]: undefined;
+};
+
 export type AuthStackParamList = {
-  [AuthRoutes.SIGN_UP]: undefined;
+  [AuthRoutes.SIGN_UP]: NavigatorScreenParams<SignUpStackParamList>;
   [AuthRoutes.SIGN_IN]: undefined;
 };
 
 export const AuthStack = createStackNavigator<AuthStackParamList>();
+
+export const SignUpStack = createStackNavigator<SignUpStackParamList>();
